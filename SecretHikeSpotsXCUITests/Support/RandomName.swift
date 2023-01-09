600a6bc1d1ab6ff7782f75e3099a84f9dff81a86
Gail Phillips <gsmyname99@gmail.com>
2023-01-09 18:27:13 +0000

    adding in comment for use

--- SecretHikeSpotsXCUITests/Support/RandomName.swift
+++ SecretHikeSpotsXCUITests/Support/RandomName.swift
@@ -2,6 +2,7 @@
 import Foundation
 import XCTest
 
+//used to create a random name for dropped pin
 func randomString(length: Int) -> String {
   let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
   return String((0..<length).map{ _ in letters.randomElement()! })
